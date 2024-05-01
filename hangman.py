b1f6a72d4c6d6b6a3b434d5c3b43220a9df3a478
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-05-01 20:57:41 +0100

    main() to map out the basic game flow

--- hangman.py
+++ hangman.py
@@ -24,3 +24,28 @@ def validate_input(player_input):
 
 def join_guessed_letters(guessed_letters):
     return " ".join(sorted(guessed_letters))
+
+def main():
+    selected_word = select_word()
+    guessed_letters = set()
+    tries = 6
+    print("Welcome to Hangman!")
+    print("You have 6 tries to guess the word.") 
+    print("Good luck!")
+    print("_ " * len(selected_word))
+    while tries > 0:
+        player_input = get_player_input()
+        if player_input in selected_word:
+            guessed_letters.add(player_input)
+            print(f"Correct! {join_guessed_letters(guessed_letters)}")
+        else:
+            tries -= 1
+            print(f"Incorrect! {join_guessed_letters(guessed_letters)}")
+            print(f"Tries left: {tries}")
+        if guessed_letters == set(selected_word):
+            print(f"Congratulations! You have guessed the word: {selected_word}")
+            break
+    else:
+        print(f"Sorry! You have run out of tries. The word was: {selected_word}")
+
+main()
