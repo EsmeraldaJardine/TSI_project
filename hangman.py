54f1a7df9c1396df56d5f1269b926eab081acb7c
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-05-01 21:41:14 +0100

    abstracting some more

--- hangman.py
+++ hangman.py
@@ -2,6 +2,8 @@ from random import choice
 import string
 from Hangman_Drawing import hangman_drawings
 
+MAX_ATTEMPTS = 6
+
 def welcome_message(selected_word):
     print("Welcome to Hangman!")
     print("You have 6 tries to guess the word.") 
@@ -39,28 +41,32 @@ def join_guessed_letters(selected_word, guessed_letters):
             result.append('_')
     return ' '.join(result)
 
-def main():
-    selected_word = select_word()
-    guessed_letters = set()
-    tries = 6
-    wrong_guesses = 0
-    hangman_drawings(wrong_guesses)
-    welcome_message(selected_word)
-    while tries > 0:
+def game_loop(selected_word, guessed_letters, attempts, wrong_guesses):
+    while attempts > 0:
         player_input = get_player_input()
         if player_input in selected_word:
             guessed_letters.add(player_input)
             print(f"Correct! {join_guessed_letters(selected_word, guessed_letters)}")
         else:
-            tries -= 1
+            attempts -= 1
             wrong_guesses += 1
             hangman_drawings(wrong_guesses)
             print(f"Incorrect! {join_guessed_letters(selected_word, guessed_letters)}")
-            print(f"Tries left: {tries}")
+            print(f"Tries left: {attempts}")
         if guessed_letters == set(selected_word):
             print(f"Congratulations! You have guessed the word: {selected_word}")
             break
     else:
         print(f"Sorry! You have run out of tries. The word was: {selected_word}")
+
+
+def main():
+    selected_word = select_word()
+    guessed_letters = set()
+    attempts = MAX_ATTEMPTS
+    wrong_guesses = 0
+    hangman_drawings(wrong_guesses)
+    welcome_message(selected_word)
+    game_loop(selected_word, guessed_letters, attempts, wrong_guesses)
 
 main()
