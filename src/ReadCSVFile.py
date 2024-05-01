f6b0199fc4301a78d29e49d3bb9072f05146450b
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-05-01 23:01:41 +0100

    adding customers

---
+++ src/ReadCSVFile.py
@@ -0,0 +1,21 @@
+import csv, os
+class ReadCSVFile:
+
+    def fix_working_directory(self):
+        current_working_directory = os.getcwd()
+        while "test" in current_working_directory or "src" in current_working_directory:
+            os.chdir("../")
+            current_working_directory = os.getcwd()
+
+    def get_file_data(self,file_name):
+        self.fix_working_directory()
+        file_data = []
+        with open("resource/" + file_name, 'rt')as data_file:
+            file_reader = csv.reader(data_file)
+            for row in file_reader:
+                file_data.append(row)
+        return file_data
+    
+    def get_last_lines(self, file_name, number_of_lines):
+        return self.get_file_data(file_name)[-1 * number_of_lines]
+
