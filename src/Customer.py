ae44a7c08b6fd32e3b428a051f447f0b5faf18d6
Esmeralda Espinoza-Jardine <esmeralda.espinoza-jardine@woodmac.com>
2024-05-02 16:52:54 +0100

    WIP adding total games won and lost attribute

--- src/Customer.py
+++ src/Customer.py
@@ -4,12 +4,16 @@ class Customer:
     last_name_position = 2
     email_position = 0
     password_position = 3
+    games_won_position = 4
+    games_lost_position = 5
 
     def __init__(self, raw_customer):
         self.first_name = raw_customer[self.first_name_position]
         self.last_name = raw_customer[self.last_name_position]
         self.email = raw_customer[self.email_position]
         self.password = raw_customer[self.password_position]
+        self.total_games_won = raw_customer[self.games_won_position]
+        self.total_games_lost= raw_customer[self.games_lost_position]
 
     def get_first_name(self):
         return self.first_name
@@ -25,3 +29,9 @@ class Customer:
 
     def compare_password(self, password):
         return password == self.password
+    
+    def get_total_games_won(self):
+        return self.total_games_won
+    
+    def get_total_games_lost(self):
+        return self.total_games_lost
