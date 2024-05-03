cd32e6e9b7288d3093064ce3ae93f8045731b07b
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-05-03 21:01:08 +0100

    getting rid of global variables and implementing the write to csv to update the wins and losses

--- main.py
+++ main.py
@@ -6,23 +6,31 @@ from src.WriteCSVFile import UpdateCsvWinsLosses
 class Main:
     def main():
         log_in = LogIn()
-        logged_in_customer = log_in.log_in()
-        print("customer email: ", logged_in_customer.get_email())
-        while is_ready_to_play() == True:
+        log_in.log_in()
+        total_wins = 0
+        total_losses = 0
+        while is_ready_to_play(total_wins, total_losses) == True:
             selected_word = select_word()
+            GUESSED_LETTERS.clear()
             hangman_drawings(WRONG_GUESSES_START)
             welcome_message(selected_word)
-            game_loop(selected_word, GUESSED_LETTERS, MAX_ATTEMPTS, WRONG_GUESSES_START)
+            
+            victories = game_loop(selected_word, GUESSED_LETTERS, MAX_WRONG_GUESSES, WRONG_GUESSES_START)
             write_to_csvfile = UpdateCsvWinsLosses()
             
-            if SESSION_WINS != 0:
-                write_to_csvfile.update_customer_game_result(logged_in_customer.get_email(), "customer.csv", True)
-            elif SESSION_LOSSES != 0:
-                write_to_csvfile.update_customer_game_result(logged_in_customer.get_email(), "customer.csv", False)
+            if victories == 1:
+                total_wins += 1
+                write_to_csvfile.update_customer_game_result(log_in.email_address, "customer.csv", True)
+                print("updated win")   
+
+            else:
+                total_losses += 1
+                write_to_csvfile.update_customer_game_result(log_in.email_address, "customer.csv", False)
+                print("updated loss")
+    
             if is_playing_again():
                 continue
             else:
-                
                 break  
 
 if __name__ == '__main__':
