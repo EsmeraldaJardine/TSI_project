39095b1bc41488f51bcc131495610e24825c3ce8
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-05-06 20:27:11 +0100

    tweaking the stub class

--- test/StubReadCSVFile.py
+++ test/StubReadCSVFile.py
@@ -1,4 +1,8 @@
 class StubReadCSVFile:
+
+    def fix_working_directory(self):
+        pass
+
     def get_file_data(self, file_name):
         if file_name == "test.csv":
 
@@ -8,3 +12,13 @@ class StubReadCSVFile:
                  ['test@test', 'Mr', 'Test', 100, 100],
                  ['a', 'a', 'a', 0, 0],
             ]
+        else:
+            return None
+        
+    def get_last_lines(self, file_name, number_of_lines):
+        data= self.get_file_data(file_name)
+        if data is not None:
+            return data[-1 * number_of_lines]
+        else:
+            return None
+        
